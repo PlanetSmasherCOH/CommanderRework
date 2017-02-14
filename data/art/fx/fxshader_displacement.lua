78de5ef9b1b98b5328de889a4fa1f13a1b7f4e83
Planet Smasher <vohcks@gmx.de>
2017-02-14 22:35:34 +0100

    Lots of unfinished stuff

---
+++ data/art/fx/fxshader_displacement.lua
@@ -0,0 +1,32 @@
+fxtypes =  
+{
+	{
+		effect = "fxshader_displacement",
+		fx =  
+		{
+			properties =  
+			{
+				property_00 =  
+				{
+					name = "tex0",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_01 =  
+				{
+					name = "TileX",
+					type = "VARTYPE_INT",
+					value = 1,
+				},
+				property_02 =  
+				{
+					name = "TileY",
+					type = "VARTYPE_INT",
+					value = 1,
+				}
+			},
+			style = "DISPLACEMENTSHADER",
+		},
+	},
+}
+
